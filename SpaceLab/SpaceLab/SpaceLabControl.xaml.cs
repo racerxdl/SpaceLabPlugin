29a00b4f9c4ec10a43adeb86b98d90e54c8c305e
Lucas Teske <Lucas Teske@ENTERPRISE>
2022-08-27 16:39:27 -0300

    Adicionar arquivos de projeto.

---
+++ SpaceLab/SpaceLab/SpaceLabControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SpaceLab
+{
+    public partial class SpaceLabControl : UserControl
+    {
+
+        private SpaceLab Plugin { get; }
+
+        private SpaceLabControl()
+        {
+            InitializeComponent();
+        }
+
+        public SpaceLabControl(SpaceLab plugin) : this()
+        {
+            Plugin = plugin;
+            DataContext = plugin.Config;
+        }
+
+        private void SaveButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Plugin.Save();
+        }
+    }
+}
